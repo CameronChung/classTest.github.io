90c1d5c58ea499ef5236440cd77da57edc68cddd
CameronChung <73194885+CameronChung@users.noreply.github.com>
2022-10-06 10:57:12 -0700

    building randomizer code

---
+++ Randomizer/sketch.js
@@ -0,0 +1,37 @@
+// my favorite sanrio characters
+let randomIndex;
+let sanrio = [{
+    name:"Kuromi", 
+    color:"black"
+}, {
+    name:"myMelody", 
+    color:"pink"
+}, {
+    name:"Cinnamoroll", 
+    color:"white"
+}, {
+    name:"Kiki", 
+    color:"blue"
+}, {
+    name:"Lala", 
+    color:"pink"
+}, {
+    name:"Hello Kitty", 
+    coloe:"white"
+}];
+
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+
+}
+function draw() {
+
+}
+
+function mousePressed() {
+    background(random(200, 255));
+    randomIndex = int(random(sanrio.length));
+    Text(sanrio[randomIndex].name, 50, 50);
+    sanrio.splice(randomIndex, 1);
+}
