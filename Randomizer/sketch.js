49e2a80f84c2668e8f9d9a4c6114657a4ec2a39c
CameronChung <73194885+CameronChung@users.noreply.github.com>
2022-10-10 18:56:59 -0700

    2b.8 updated Randomizer

--- Randomizer/sketch.js
+++ Randomizer/sketch.js
@@ -1,5 +1,6 @@
 // my favorite sanrio characters
 let randomIndex;
+let animating = false;
 let sanrio = [{
     name:"Kuromi", 
     color:"black"
@@ -23,16 +24,36 @@ let sanrio = [{
 function setup() {
     createCanvas(600, 600);
     background(200);
+    textSize(32);
+
+    text("Click to Randomize", 50, 50);
 }
 
 function draw() {
 
+    if(animating == true){
+        ellipse(random(width), random(height), random(50, 200));
+    }
+
+}
+
+function randomizer(){
+    animating = false;
+    if(sanrio[0]){
+    // dispays random name and splices it out if the array
+    background(random(200, 255));
+    randomIndex = int(random(sanrio.length));
+    text(`${sanrio[randomIndex].name}'s color is ${sanrio[randomIndex].color}`, 50, 50);
+    //text(sanrio[randomIndex].name + "'s color is " + sanrio[randomIndex].color, 50, 50);
+    sanrio.splice(randomIndex, 1);
+    } else{
+        background(random(200, 255));
+        text("nothing left!", 50, 50);
+    }
 }
 
 function mousePressed() {
-    background(random(200, 255));
-    randomIndex = int(random(sanrio.length));
-    text(sanrio[randomIndex].name, 50, 50);
-    console.log(sanrio[randomIndex]);
-    sanrio.splice(randomIndex, 1);
+    animating = true;
+    setTimeout(randomizer, 2000);
+
 }
